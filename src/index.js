0459eeaf74fb1867eb3006e7df72ea9eaeb77ff0
Andy Mardell <andy@sidigital.co>
2019-11-27 13:07:44 +0000

    Restructure CRA

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-serviceWorker.unregister()
