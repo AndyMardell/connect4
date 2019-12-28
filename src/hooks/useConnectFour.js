4cac565fb171becdb0ab05d9ad8a107554493532
Andy Mardell <andy@sidigital.co>
2019-12-28 12:43:58 +0000

    Erm, it works?

--- src/hooks/useConnectFour.js
+++ src/hooks/useConnectFour.js
@@ -19,28 +19,26 @@ const useConnectFour = () => {
     return rows
   }
 
-  const getDiagonals = grid => {
+  const getDiagonals = (grid, reverse = false) => {
     const colDiagonals = []
     const rowDiagonals = []
 
-    const rows = getRows(grid)
+    const rows = reverse ? getRows(grid).reverse() : getRows(grid)
     const columns = getColumns(grid)
 
     columns.forEach((_, coli) => {
       colDiagonals[coli] = []
-      rows.forEach((_, rowi) => {
-        const cell = (coli + rowi) * 8
-        if (cell > grid.length) return
-        colDiagonals[coli] = [...colDiagonals[coli], grid[cell]]
+      rows.forEach((row, rowi) => {
+        if (coli + rowi > row.length) return
+        colDiagonals[coli] = [...colDiagonals[coli], row[coli + rowi]]
       })
     })
 
     rows.forEach((_, rowi) => {
       rowDiagonals[rowi] = []
-      columns.forEach((_, coli) => {
-        const cell = (rowi + coli) * 8
-        if (cell > grid.length) return
-        rowDiagonals[rowi] = [...rowDiagonals[rowi], grid[cell]]
+      columns.forEach((column, coli) => {
+        if (coli + rowi > column.length) return
+        rowDiagonals[rowi] = [...rowDiagonals[rowi], column[coli + rowi]]
       })
     })
 
@@ -50,7 +48,8 @@ const useConnectFour = () => {
   const getLines = grid => [
     ...getColumns(grid),
     ...getRows(grid),
-    ...getDiagonals(grid)
+    ...getDiagonals(grid),
+    ...getDiagonals(grid, true)
   ]
 
   return { getLines }
